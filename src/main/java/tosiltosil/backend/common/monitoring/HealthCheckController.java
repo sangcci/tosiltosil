e81e07047ec37930769d277e5e261cccb4079370
sangcci <pshsh910@gmail.com>
2025-04-28 18:33:14 +0900

    setting: set swagger

---
+++ src/main/java/tosiltosil/backend/common/monitoring/HealthCheckController.java
@@ -0,0 +1,15 @@
+package tosiltosil.backend.common.monitoring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+}
