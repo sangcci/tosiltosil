e9dd8cc7a4ac3bbba3d32d235145a69d50dd5325
sangcci <pshsh910@gmail.com>
2025-04-28 18:47:12 +0900

    feat: add api specification in HealthCheckController
    to separate controller code

--- src/main/java/tosiltosil/backend/common/monitoring/HealthCheckController.java
+++ src/main/java/tosiltosil/backend/common/monitoring/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/health")
-public class HealthCheckController {
+public class HealthCheckController implements HealthCheckApiSpecification {
 
     @GetMapping
     public String healthCheck() {
