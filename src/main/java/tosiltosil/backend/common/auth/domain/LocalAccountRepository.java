0933d95a2f9f12dde63c7139bf066d0af016f243
sangcci <pshsh910@gmail.com>
2025-06-05 22:07:54 +0900

    fix: LocalAccountRepository에서 findByEmail() 제거

--- src/main/java/tosiltosil/backend/common/auth/domain/LocalAccountRepository.java
+++ src/main/java/tosiltosil/backend/common/auth/domain/LocalAccountRepository.java
@@ -1,9 +1,7 @@
 package tosiltosil.backend.common.auth.domain;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocalAccountRepository extends JpaRepository<LocalAccount, Long> {
 
-    Optional<LocalAccount> findByEmail(String email);
 }
