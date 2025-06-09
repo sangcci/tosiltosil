aa9515d7319a18695ac038b37a18ffe8464ddbd4
sangcci <pshsh910@gmail.com>
2025-06-09 19:29:36 +0900

    test: Spring 테스트 Support class 추가

---
+++ src/test/java/tosiltosil/backend/support/IntegrationTestSupport.java
@@ -0,0 +1,11 @@
+package tosiltosil.backend.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class IntegrationTestSupport {
+
+}
