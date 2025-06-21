72a9837892a35032f7d453af908dd6648b4ad2c4
hy5sun <ssunykkim@naver.com>
2025-06-21 09:12:20 +0900

    test: TermsService 테스트 코드 추가

---
+++ src/test/java/tosiltosil/backend/module/terms/application/TermsServiceTest.java
@@ -0,0 +1,117 @@
+package tosiltosil.backend.module.terms.application;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+import tosiltosil.backend.common.domain.exception.BadRequestException;
+import tosiltosil.backend.common.domain.exception.NotFoundException;
+import tosiltosil.backend.module.terms.domain.TermsRepository;
+import tosiltosil.backend.module.terms.domain.request.TermsDetail;
+import tosiltosil.backend.module.terms.infrastructure.MemberTermsJpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class TermsServiceTest {
+
+    @InjectMocks
+    private TermsService termsService;
+
+    @Mock
+    private TermsRepository termsRepository;
+
+    @Mock
+    private MemberTermsJpaRepository memberTermsJpaRepository;
+
+    private List<TermsDetail> createValidTerms() {
+        return List.of(
+                new TermsDetail("termsOfService", "0.1.0", true),
+                new TermsDetail("privacyPolicy", "0.1.0", true),
+                new TermsDetail("ageConfirmation", "0.1.0", true)
+        );
+    }
+
+    @Test
+    void 정상_약관_검증_성공() {
+        // given
+        List<TermsDetail> terms = createValidTerms();
+
+        terms.forEach(t -> {
+            when(termsRepository.findTermsIsRequired(t.title(), t.version()))
+                    .thenReturn(Optional.of(true));
+        });
+
+        // when & then
+        assertDoesNotThrow(() -> termsService.validateTerms(terms));
+    }
+
+    @Test
+    void 약관_저장_성공() {
+        // given
+        UUID memberId = UUID.randomUUID();
+        List<TermsDetail> terms = createValidTerms();
+
+        terms.forEach(t -> {
+            when(termsRepository.findVersionId(t.title(), t.version()))
+                    .thenReturn(Optional.of(1L));
+                }
+        );
+
+        // when
+        termsService.saveTerms(memberId, terms);
+
+        // then
+        verify(memberTermsJpaRepository, times(terms.size())).save(any());
+    }
+
+    @Test
+    void 필수_약관_미동의_예외처리() {
+        //given
+        List<TermsDetail> terms = List.of(
+                new TermsDetail("termsOfService", "0.1.0", false),
+                new TermsDetail("privacyPolicy", "0.1.0", true),
+                new TermsDetail("ageConfirmation", "0.1.0", true)
+        );
+
+        when(termsRepository.findTermsIsRequired("termsOfService", "0.1.0"))
+                .thenReturn(Optional.of(true));
+
+        // when & then
+        BadRequestException exception = assertThrows(
+                BadRequestException.class,
+                () -> termsService.validateTerms(terms)
+        );
+
+        assertEquals("필수 약관에 대해 동의하지 않았습니다.", exception.getMessage());
+    }
+
+    @Test
+    void 약관_정보가_없을_경우_예외처리() {
+        //given
+        List<TermsDetail> terms = List.of(
+                new TermsDetail("잘못된_약관", "0.0.1", true),
+                new TermsDetail("privacyPolicy", "0.1.0", true),
+                new TermsDetail("ageConfirmation", "0.1.0", true)
+        );
+
+        when(termsRepository.findTermsIsRequired("잘못된_약관", "0.0.1"))
+                .thenReturn(Optional.empty());
+
+        // when & then
+        NotFoundException exception = assertThrows(
+                NotFoundException.class,
+                () -> termsService.validateTerms(terms)
+        );
+
+        assertEquals("약관을 찾을 수 없습니다.", exception.getMessage());
+    }
+}
