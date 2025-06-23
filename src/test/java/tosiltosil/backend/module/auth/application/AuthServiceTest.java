9e8504b6bff125f5a9bf3d2d310932713d456a8a
hy5sun <ssunykkim@naver.com>
2025-06-24 02:11:36 +0900

    refactor: AuthServiceTest에서 회원가입 테스트 코드 삭제

--- src/test/java/tosiltosil/backend/module/auth/application/AuthServiceTest.java
+++ src/test/java/tosiltosil/backend/module/auth/application/AuthServiceTest.java
@@ -11,19 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 import tosiltosil.backend.common.domain.exception.ConflictException;
 import tosiltosil.backend.module.auth.domain.request.CreateLocalMemberRequest;
 import tosiltosil.backend.module.member.application.MemberService;
-import tosiltosil.backend.module.member.domain.LocalAccount;
-import tosiltosil.backend.module.member.domain.Member;
 import tosiltosil.backend.module.member.domain.value.LoginType;
 import tosiltosil.backend.module.terms.application.TermsService;
-import tosiltosil.backend.module.terms.domain.request.TermsDetail;
-
-import java.util.List;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
@@ -44,55 +36,6 @@ class AuthServiceTest {
 
     @Mock
     private MultipartFile profileImage;
-
-    private List<TermsDetail> createValidTerms() {
-        return List.of(
-                new TermsDetail("termsOfService", "0.1.0", true),
-                new TermsDetail("privacyPolicy", "0.1.0", true),
-                new TermsDetail("ageConfirmation", "0.1.0", true)
-        );
-    }
-
-    private CreateLocalMemberRequest mockValidRequest(String email, String password, List<TermsDetail> terms) {
-        CreateLocalMemberRequest request = mock(CreateLocalMemberRequest.class);
-        when(request.email()).thenReturn(email);
-        when(request.password()).thenReturn(password);
-        when(request.terms()).thenReturn(terms);
-        return request;
-    }
-
-    @Test
-    void LOCAL_회원가입_성공() {
-        //given
-        String email = "test@example.com";
-        String password = "qwer1234!";
-        String encryptedPassword = "ENC_qwer1234!";
-        String code = "ABC123";
-
-        UUID memberId = UUID.randomUUID();
-        List<TermsDetail> terms = createValidTerms();
-
-        CreateLocalMemberRequest request = mockValidRequest(email, password, terms);
-        Member member = mock(Member.class);
-        LocalAccount localAccount = mock(LocalAccount.class);
-
-        when(memberService.generateRandomCode()).thenReturn(code);
-        when(request.toMemberEntities(eq(code), anyString())).thenReturn(member);
-        when(member.getId()).thenReturn(memberId);
-
-        when(passwordEncoder.encode(password)).thenReturn(encryptedPassword);
-        when(request.toLocalAccountEntities(eq(memberId), eq(encryptedPassword))).thenReturn(localAccount);
-
-        // when
-        authService.localSignUp(request, profileImage);
-
-        // then
-        verify(termsService).validateTerms(terms);
-        verify(memberService).validateEmail(email, LoginType.LOCAL);
-        verify(memberService).saveMember(member);
-        verify(memberService).saveLocalAccount(localAccount);
-        verify(termsService).saveTerms(memberId, terms);
-    }
 
     @Test
     void 중복_이메일_회원가입_실패() {
