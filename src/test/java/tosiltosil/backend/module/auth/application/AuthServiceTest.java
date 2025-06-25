6cc714db3f716adc6788b21cc617ff390ac65209
hy5sun <ssunykkim@naver.com>
2025-06-25 17:43:48 +0900

    refactor: 이메일 중복 예외 발생 시 중단 여부 테스트 코드 삭제

--- src/test/java/tosiltosil/backend/module/auth/application/AuthServiceTest.java
+++ src/test/java/tosiltosil/backend/module/auth/application/AuthServiceTest.java
@@ -1,6 +1,5 @@
 package tosiltosil.backend.module.auth.application;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -8,16 +7,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
-import tosiltosil.backend.common.domain.exception.ConflictException;
-import tosiltosil.backend.module.auth.domain.request.CreateLocalMemberRequest;
 import tosiltosil.backend.module.member.application.MemberService;
-import tosiltosil.backend.module.member.domain.value.LoginType;
 import tosiltosil.backend.module.terms.application.TermsService;
-
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -38,27 +29,4 @@ class AuthServiceTest {
     @Mock
     private MultipartFile profileImage;
 
-    @Test
-    void 이메일_중복_예외가_발생하면_로직을_중지한다() {
-        //given
-        CreateLocalMemberRequest request = mock(CreateLocalMemberRequest.class);
-        String email = "duplicate@example.com";
-
-        when(request.email()).thenReturn(email);
-
-        doThrow(new ConflictException("이미 등록된 이메일입니다."))
-                .when(memberService).validateEmail(email, LoginType.LOCAL);
-
-        // when & then
-        Throwable thrown = catchThrowable(() -> authService.localSignUp(request, profileImage));
-
-        assertThat(thrown)
-                .isInstanceOf(ConflictException.class)
-                .hasMessage("이미 등록된 이메일입니다.");
-
-        verify(memberService, never()).generateRandomCode();
-        verify(memberService, never()).saveMember(any());
-        verify(memberService, never()).saveLocalAccount(any());
-        verify(termsService, never()).saveTerms(any(UUID.class), any());
-    }
 }
