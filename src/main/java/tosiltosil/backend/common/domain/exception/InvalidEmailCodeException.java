ab91bf8ba0a419561957efb71810410984b94ef9
hy5sun <ssunykkim@naver.com>
2025-06-26 14:46:34 +0900

    feat: 인증번호 검증 실패 예외 추가 및 ExceptionHandler 처리 로직 구현

---
+++ src/main/java/tosiltosil/backend/common/domain/exception/InvalidEmailCodeException.java
@@ -0,0 +1,14 @@
+package tosiltosil.backend.common.domain.exception;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidEmailCodeException extends RuntimeException {
+
+    private final int failCount;
+
+    public InvalidEmailCodeException(int failCount) {
+        super("금일 총 " + failCount + "회 틀렸습니다. 하루 최대 5회까지 가능합니다.");
+        this.failCount = failCount;
+    }
+}
