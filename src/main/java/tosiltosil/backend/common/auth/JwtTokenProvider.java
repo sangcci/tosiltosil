25d9ada780cd1a49643e72f263eb170da06d25b5
hy5sun <ssunykkim@naver.com>
2025-06-28 23:59:17 +0900

    feat: JwtTokenProvider 클래스 추가 - JWT 토큰 생성 및 검증 메서드 구현 - Redis를 통한 이메일 및 리프레시 토큰 관리 기능 추가 - AccessToken & RefreshToken 발급 및 갱신 로직 구현

---
+++ src/main/java/tosiltosil/backend/common/auth/JwtTokenProvider.java
@@ -0,0 +1,131 @@
+package tosiltosil.backend.common.auth;
+
+import lombok.RequiredArgsConstructor;
+import tosiltosil.backend.common.auth.domain.response.AccessTokenInfo;
+import tosiltosil.backend.common.auth.domain.response.RefreshTokenInfo;
+import tosiltosil.backend.common.auth.domain.response.TemporaryTokenInfo;
+import tosiltosil.backend.common.auth.domain.response.TokenPair;
+import tosiltosil.backend.common.util.JwtUtil;
+import tosiltosil.backend.common.domain.exception.UnauthorizedException;
+import tosiltosil.backend.module.auth.infrastructure.EmailRedisRepository;
+import tosiltosil.backend.module.auth.infrastructure.RefreshTokenRedisRepository;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    private final EmailRedisRepository emailRedisRepository;
+
+    private static final long RENEWAL_THRESHOLD = 24 * 60 * 60 * 1000L; // 24시간 (하루)
+
+    public String createTemporaryToken(UUID memberId, String cacheKey) {
+        saveEmailToRedis(cacheKey);
+        return jwtUtil.generateTemporaryToken(memberId, cacheKey);
+    }
+
+    public String reissueAccessToken(String refreshToken) {
+        RefreshTokenInfo tokenInfo = retrieveRefreshToken(refreshToken);
+        return createAccessToken(tokenInfo.memberId());
+    }
+
+    public TokenPair reissueAllToken(String accessToken, String refreshToken) {
+
+        RefreshTokenInfo tokenInfo = retrieveRefreshToken(refreshToken);
+        UUID memberId = tokenInfo.memberId();
+
+        long now = System.currentTimeMillis();
+        long expiration = getRefreshTokenExpirationTime(refreshToken);
+
+        if (expiration - now < RENEWAL_THRESHOLD) {
+            deleteRefreshTokenFromRedis(memberId);
+            return createTokenPair(memberId);
+        }
+
+        String newAccessToken = createAccessToken(memberId);
+        return TokenPair.of(newAccessToken, refreshToken);
+    }
+
+    public TokenPair createTokenPair(UUID memberId) {
+        String accessToken = createAccessToken(memberId);
+        String refreshToken = createRefreshToken(memberId);
+        return TokenPair.of(accessToken, refreshToken);
+    }
+
+    public String retrieveEmail(String temporaryToken) {
+        TemporaryTokenInfo tokenInfo = jwtUtil.parseTemporaryToken(temporaryToken);
+        String email = getEmailFromRedis(tokenInfo.key());
+
+        if (email == null)
+            throw new UnauthorizedException("올바르지 않은 임의 토큰입니다.");
+
+        return email;
+    }
+
+    public AccessTokenInfo retrieveAccessToken(String accessToken) {
+        try {
+            return jwtUtil.parseAccessToken(accessToken);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public RefreshTokenInfo retrieveRefreshToken(String refreshToken) {
+        RefreshTokenInfo tokenInfo = jwtUtil.parseRefreshToken(refreshToken);
+        String redisToken = getRefreshTokenFromRedis(tokenInfo.memberId());
+
+        if (redisToken == null || !redisToken.equals(tokenInfo.token())) {
+            throw new UnauthorizedException("올바르지 않은 리프레시 토큰입니다.");
+        }
+
+        return tokenInfo;
+    }
+
+    public void saveEmailToRedis(String email) {
+        Long expiration = jwtUtil.getTemporaryTokenExpiration(email);
+        long now = System.currentTimeMillis();
+        long ttl = expiration - now;
+
+        emailRedisRepository.save(UUID.randomUUID(), email, ttl);
+    }
+
+    public void saveRefreshTokenToRedis(UUID memberId, String refreshToken) {
+        Long expiration = jwtUtil.getRefreshTokenExpiration(refreshToken);
+        long now = System.currentTimeMillis();
+        long ttl = expiration - now;
+
+        refreshTokenRedisRepository.save(memberId, refreshToken, ttl);
+    }
+
+    private String createAccessToken(UUID memberId)  {
+        return jwtUtil.generateAccessToken(memberId);
+    }
+
+    private String createRefreshToken(UUID memberId) {
+        String refreshToken = jwtUtil.generateRefreshToken(memberId);
+        saveRefreshTokenToRedis(memberId, refreshToken);
+        return refreshToken;
+    }
+
+    private Long getRefreshTokenExpirationTime(String refreshToken) {
+        return jwtUtil.getRefreshTokenExpiration(refreshToken);
+    }
+
+    private String getEmailFromRedis(UUID cacheKey) {
+        return emailRedisRepository.get(cacheKey);
+    }
+
+    private String getRefreshTokenFromRedis(UUID memberId) {
+        return refreshTokenRedisRepository.get(memberId);
+    }
+
+    private void deleteEmailFromRedis(UUID cacheKey) {
+        emailRedisRepository.delete(cacheKey);
+    }
+
+    private void deleteRefreshTokenFromRedis(UUID memberId) {
+        refreshTokenRedisRepository.delete(memberId);
+    }
+}
