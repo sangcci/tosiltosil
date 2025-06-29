9fb09583271d5e90f3c5e9b932c9671c7836a610
hy5sun <ssunykkim@naver.com>
2025-06-29 11:44:02 +0900

    refactor: CookieUtil 메서드 추가 - AccessToken 전용 쿠키 생성 메서드 추가 - 기존 메서드명 수정

--- src/main/java/tosiltosil/backend/common/util/CookieUtil.java
+++ src/main/java/tosiltosil/backend/common/util/CookieUtil.java
@@ -19,7 +19,7 @@ public class CookieUtil {
     private static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     private static final String TEMPORARY_TOKEN_COOKIE_NAME = "temporary_token";
 
-    public HttpHeaders generateAuthTokenCookies(String accessToken, String refreshToken) {
+    public HttpHeaders generateAccessAndRefreshTokenCookies(String accessToken, String refreshToken) {
         ResponseCookie accessTokenCookie = generateCookie(ACCESS_TOKEN_COOKIE_NAME, accessToken);
         ResponseCookie refreshTokenCookie = generateCookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
 
@@ -27,6 +27,14 @@ public class CookieUtil {
         headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
         headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
+        return headers;
+    }
+
+    public HttpHeaders generateAccessTokenCookies(String accessToken) {
+        ResponseCookie accessTokenCookie = generateCookie(ACCESS_TOKEN_COOKIE_NAME, accessToken);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
         return headers;
     }
 
