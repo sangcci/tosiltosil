fa8e400aa463a29faf16fdb123bd8ca5987853b6
hy5sun <ssunykkim@naver.com>
2025-06-30 01:29:38 +0900

    feat: Spring Security 설정에 Jwt 및 CORS 추가 - JWT 인증 처리를 위한 JwtAuthFilter 추가 - JwtTokenProvider 및 CookieUtil 주입 및 설정 - CORS 설정 추가 및 기본 설정 적용

--- src/main/java/tosiltosil/backend/common/auth/config/SecurityConfig.java
+++ src/main/java/tosiltosil/backend/common/auth/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -14,11 +15,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import tosiltosil.backend.common.auth.JwtTokenProvider;
+import tosiltosil.backend.common.auth.filter.JwtAuthFilter;
+import tosiltosil.backend.common.util.CookieUtil;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CookieUtil cookieUtil;
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -26,6 +39,7 @@ public class SecurityConfig {
                 .csrf(CsrfConfigurer<HttpSecurity>::disable)
                 .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
                 .httpBasic(HttpBasicConfigurer<HttpSecurity>::disable)
+                .cors(Customizer.withDefaults())
                 .headers(it -> it.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
@@ -33,7 +47,32 @@ public class SecurityConfig {
                         // swagger
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().permitAll())
+                .addFilterBefore(
+                        jwtAuthFilter(jwtTokenProvider, cookieUtil),
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(JwtTokenProvider jwtTokenProvider, CookieUtil cookieUtil) {
+        return new JwtAuthFilter(jwtTokenProvider, cookieUtil);
     }
 
     @Bean
