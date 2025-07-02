33a0e79f5d603e917e3b2dc1d76067394a31e065
hy5sun <ssunykkim@naver.com>
2025-07-02 19:18:32 +0900

    refactor: AccessTokenInfo 및 RefreshTokenInfo 내 불필요한 필드 삭제

--- src/main/java/tosiltosil/backend/common/auth/domain/response/AccessTokenInfo.java
+++ src/main/java/tosiltosil/backend/common/auth/domain/response/AccessTokenInfo.java
@@ -1,24 +1,15 @@
 package tosiltosil.backend.common.auth.domain.response;
 
 import io.jsonwebtoken.Claims;
-import tosiltosil.backend.common.auth.domain.value.TokenType;
 
 import java.util.UUID;
 
 public record AccessTokenInfo(
         UUID memberId,
-        String token,
-        TokenType tokenType
+        String token
 ) {
-    public static AccessTokenInfo of(UUID memberId, String token) {
-        return new AccessTokenInfo(memberId, token, TokenType.ACCESS);
-    }
-
     public static AccessTokenInfo from(String token, Claims claims) {
-        return new AccessTokenInfo(
-                UUID.fromString(claims.getSubject()),
-                token,
-                TokenType.valueOf(claims.get("type", String.class))
-        );
+        UUID memberId = UUID.fromString(claims.getSubject());
+        return new AccessTokenInfo(memberId, token);
     }
 }
--- src/main/java/tosiltosil/backend/common/auth/domain/response/RefreshTokenInfo.java
+++ src/main/java/tosiltosil/backend/common/auth/domain/response/RefreshTokenInfo.java
@@ -1,24 +1,15 @@
 package tosiltosil.backend.common.auth.domain.response;
 
 import io.jsonwebtoken.Claims;
-import tosiltosil.backend.common.auth.domain.value.TokenType;
 
 import java.util.UUID;
 
 public record RefreshTokenInfo(
         UUID memberId,
-        String token,
-        TokenType tokenType
+        String token
 ) {
-    public static RefreshTokenInfo of(UUID memberId, String token) {
-        return new RefreshTokenInfo(memberId, token, TokenType.REFRESH);
-    }
-
     public static RefreshTokenInfo from(String token, Claims claims) {
-        return new RefreshTokenInfo(
-                UUID.fromString(claims.getSubject()),
-                token,
-                TokenType.valueOf(claims.get("type", String.class))
-        );
+        UUID memberId = UUID.fromString(claims.getSubject());
+        return new RefreshTokenInfo(memberId, token);
     }
 }
