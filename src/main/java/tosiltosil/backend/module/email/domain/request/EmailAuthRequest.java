600c75569fb47a88dc420c63ab48f3c384e4367c
hy5sun <ssunykkim@naver.com>
2025-07-07 09:54:18 +0900

    refactor: Swagger 관련 어노테이션 제거

--- src/main/java/tosiltosil/backend/module/email/domain/request/EmailAuthRequest.java
+++ src/main/java/tosiltosil/backend/module/email/domain/request/EmailAuthRequest.java
@@ -1,6 +1,5 @@
 package tosiltosil.backend.module.email.domain.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -10,7 +9,6 @@ public record EmailAuthRequest(
         @Email(message = "이메일 형식이 아닙니다.")
         String email,
 
-        @Schema(description = "인증번호 (6자리 숫자)", example = "123456", minLength = 6, maxLength = 6)
         @NotBlank(message = "인증번호를 입력해주세요.")
         @Pattern(
                 regexp = "^\\d{6}$",
