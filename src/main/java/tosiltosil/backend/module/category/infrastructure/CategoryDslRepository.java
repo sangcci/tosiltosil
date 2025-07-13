26baa6371e4152c5594b2b0436163a1275fa49c2
sangcci <pshsh910@gmail.com>
2025-07-13 20:09:45 +0900

    fix: 카테고리 날짜 별 색깔 조회 query 수정

--- src/main/java/tosiltosil/backend/module/category/infrastructure/CategoryDslRepository.java
+++ src/main/java/tosiltosil/backend/module/category/infrastructure/CategoryDslRepository.java
@@ -1,14 +1,18 @@
 package tosiltosil.backend.module.category.infrastructure;
 
-import static com.querydsl.core.types.Projections.list;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 import static tosiltosil.backend.module.category.domain.QCategory.category;
 import static tosiltosil.backend.module.goal.domain.QGoal.goal;
 
-import com.querydsl.core.group.GroupBy;
-import com.querydsl.core.types.Projections;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -24,20 +28,37 @@ public class CategoryDslRepository {
             final UUID memberId,
             final YearMonth yearMonth
     ) {
-        return queryFactory
+        // 1. 날짜 별 카테고리 색깔 모두 조회
+        List<Tuple> fetch = queryFactory
+                .selectDistinct(goal.date, category.color)
                 .from(goal)
                 .join(category)
                 .on(goal.categoryId.eq(category.id))
                 .where(
                         goal.memberId.eq(memberId),
                         goal.date.between(yearMonth.atDay(1), yearMonth.atEndOfMonth())
+                        // TODO: 순서 조건 추가 및 목표 완료 상태 조건 추가
                 )
-                .orderBy(goal.date.asc(), category.color.asc()) // 순서
-                .transform(GroupBy.groupBy(goal.date).list(
-                        Projections.constructor(CategoryColorPerDayResponse.class,
-                                goal.date,
-                                list(category.color)
+                .orderBy(goal.date.asc(), category.color.asc())
+                .fetch();
+
+        // 2. 날짜 별 색깔 grouping
+        Map<LocalDate, List<String>> grouping = fetch
+                .stream()
+                .collect(groupingBy(
+                        tuple -> Objects.requireNonNull(tuple.get(goal.date)),
+                        mapping(
+                                tuple -> Objects.requireNonNull(tuple.get(category.color)),
+                                toList()
                         )
                 ));
+
+        // 3. 색깔 2개 제한 stream 적용
+        return grouping.entrySet().stream()
+                .map(entry -> new CategoryColorPerDayResponse(
+                        entry.getKey(),
+                        entry.getValue().stream().limit(2).collect(toList())
+                ))
+                .collect(toList());
     }
 }
