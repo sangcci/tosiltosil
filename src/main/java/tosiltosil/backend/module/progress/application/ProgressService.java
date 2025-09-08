8fa6f57f5e07b24bbd510eeae9b6fd69910f2dbb
sangcci <pshsh910@gmail.com>
2025-09-09 02:44:54 +0900

    fix: progress를 조회할 때 empty일 경우 초기 Progress를 생성하고 저장하도록 변경.
    - 계정 생성 시 문제가 발생한 것이므로 info log를 남겨놓음.

--- src/main/java/tosiltosil/backend/module/progress/application/ProgressService.java
+++ src/main/java/tosiltosil/backend/module/progress/application/ProgressService.java
@@ -1,10 +1,12 @@
 package tosiltosil.backend.module.progress.application;
 
 import java.time.Duration;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tosiltosil.backend.common.logging.domain.InfoLog;
 import tosiltosil.backend.module.progress.domain.Progress;
 import tosiltosil.backend.module.progress.domain.ProgressRepository;
 
@@ -21,8 +23,11 @@ public class ProgressService {
 
     @Transactional(readOnly = true)
     public Duration updateTodayDuration(final UUID memberId, final Duration duration) {
-        // TODO: empty일 경우 처리
-        Progress progress = progressRepository.findByMemberId(memberId).get();
+        Optional<Progress> progressOptional = progressRepository.findByMemberId(memberId);
+        Progress progress = progressOptional.orElseGet(() -> {
+            InfoLog.of("계정 생성 시 Progress가 생성되지 않아 지금 생성합니다. 계정 생성 부분을 확인해주세요");
+            return Progress.of(memberId, Duration.ZERO, 0);
+        });
 
         progress.addTodayDuration(duration);
         progressRepository.save(progress);
@@ -32,8 +37,11 @@ public class ProgressService {
 
     @Transactional(readOnly = true)
     public Duration subtractTodayDuration(final UUID memberId, final Duration duration) {
-        // TODO: empty일 경우 처리
-        Progress progress = progressRepository.findByMemberId(memberId).get();
+        Optional<Progress> progressOptional = progressRepository.findByMemberId(memberId);
+        Progress progress = progressOptional.orElseGet(() -> {
+            InfoLog.of("계정 생성 시 Progress가 생성되지 않아 지금 생성합니다. 계정 생성 부분을 확인해주세요");
+            return Progress.of(memberId, Duration.ZERO, 0);
+        });
 
         progress.subtractTodayDuration(duration);
         progressRepository.save(progress);
